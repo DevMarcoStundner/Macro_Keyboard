a77373dedf77d2d27e6c89d09ae35d84c414a1f0
Marco <101337732+DevMarcoStundner@users.noreply.github.com>
2023-10-20 19:42:12 +0200

    Changed type of placeing

--- Hid_gui.py
+++ Hid_gui.py
@@ -1,62 +1,37 @@
 # Imports libs 
 import tkinter as tk
 
-
-# Consts
-ROW1 = 0
-ROW2 = 1
-ROW3 = 2
-BTIPADY = 20
-BTPADX = 5
-BTPADY = 5
-
 #========================================== Window config
 window = tk.Tk()
 window.geometry("750x480")
 window.title('Macro Keyboard GUI')
 
-#========================================== Frames
-btn_frm = tk.Frame(master = window)
-btn_frm.pack()
+#========================================== Creates Frame
+content = tk.Frame(window) 
+#========================================== Creates BTN 
 
-#========================================== Creats BTN and sorts it in a grid
+content.grid(column = 0, row = 0)
 
-btn_frm.grid(row = ROW1, column = 0, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn1 = tk.Button(master = btn_frm, text = "BT1")
-btn1.pack()
+btn1 = tk.Button(content, text = "BT1")
+btn1.grid(column = 0, row = 0)
+btn2 = tk.Button(content, text = "BT2")
+btn2.grid(column = 1, row = 0)
+btn3 = tk.Button(content, text = "BT3")
+btn3.grid(column = 2, row = 0)
 
-btn_frm.grid(row = ROW1, column = 1, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn2 = tk.Button(master = btn_frm, text = "BT2")
-btn2.pack()
+btn4 = tk.Button(content, text = "BT4")
+btn4.grid(column = 0, row = 1)
+btn5 = tk.Button(content, text = "BT5")
+btn5.grid(column = 1, row = 1)
+btn6 = tk.Button(content, text = "BT6")
+btn6.grid(column = 2, row = 1)
 
-btn_frm.grid(row = ROW1, column = 2, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn3 = tk.Button(master = btn_frm, text = "BT3")
-btn3.pack()
-
-btn_frm.grid(row = ROW2, column = 0, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn4 = tk.Button(master = btn_frm, text = "BT4")
-btn4.pack()
-
-btn_frm.grid(row = ROW2, column = 1, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn5 = tk.Button(master = btn_frm, text = "BT5")
-btn5.pack()
-
-btn_frm.grid(row = ROW2, column = 2, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn6 = tk.Button(master = btn_frm, text = "BT6")
-btn6.pack()
-
-btn_frm.grid(row = ROW3, column = 0, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn7 = tk.Button(master = btn_frm, text = "BT7")
-btn7.pack()
-
-btn_frm.grid(row = ROW3, column = 1, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn8 = tk.Button(master = btn_frm, text = "BT8")
-btn8.pack()
-
-btn_frm.grid(row = ROW3, column = 2, ipady = BTIPADY, padx = BTPADX, pady = BTPADY)
-btn9 = tk.Button(master = btn_frm, text = "BT9")
-btn9.pack()
-
+btn7 = tk.Button(content, text = "BT7")
+btn7.grid(column = 0, row = 3)
+btn8 = tk.Button(content, text = "BT8")
+btn8.grid(column = 1, row = 3)
+btn9 = tk.Button(content, text = "BT9")
+btn9.grid(column = 2, row = 3)
 
 
 # main loop
